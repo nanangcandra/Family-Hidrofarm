2e3dada3b307dde1f28f3bd3a8d884fa9989780a
FauzanAzmi12181 <83965576+FauzanAzmi12181@users.noreply.github.com>
2021-06-07 10:05:16 +0700

    Create mymodel.php

---
+++ mymodel.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class mymodel extends CI_Model {
+    // public function getData($tabel){
+    //     $mydata = $this->db->query("SELECT * FROM ".$tabel);
+    //     return $mydata->result_array();
+    // }
+
+    // quey builder, pake:
+    public function getData($tabel){
+        $zn = $this->db->get($tabel);
+        return $zn->result_array();
+    }
+
+    public function masukkan($tabel, $data){
+        $zn = $this->db->insert($tabel, $data);
+        return $zn;
+    }
+
+    public function hapus($tabel, $where){
+        $zn = $this->db->delete($tabel, $where);
+        return $zn;
+    }
+
+    public function perbarui($tabel, $data, $where){
+        $zn = $this->db->update($tabel, $data, $where);
+        return $zn;
+    }
+
+    public function getData_khusus($tabel, $where){
+        $zn = $this->db->get_where($tabel, $where);
+        return $zn->result_array();
+    }
+}
