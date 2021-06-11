b62863266d5f2b04ed104d61b04f72aa5214b288
FauzanAzmi12181 <83965576+FauzanAzmi12181@users.noreply.github.com>
2021-06-11 19:48:56 +0700

    Update form_tambah.php
    Pembaruan form_tambah V2

--- form_tambah.php
+++ form_tambah.php
@@ -4,9 +4,11 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="<?php echo base_url()."index.php/hal_utama/tambah_data/" ?>" method="POST">
-        Nama Alat: <br><input type="text" name="Alat"><br><br>
-        Fungsi: <br><input type="text" name="Fungsi"><br><br>
+    <form action="<?php echo base_url()."index.php/hal_admin/tambah_data/"; ?>" method="POST">
+        Nama Barang: <br><input type="text" name="NamaBarang"><br><br>
+        Jenis Barang: <br><input type="text" name="JenisBarang"><br><br>
+        Stok: <br><input type="text" name="Stok"><br><br>
+        Harga: <br><input type="text" name="Harga"><br><br>
         <input type="submit"> <input type="reset">
     </form>
 </body>
