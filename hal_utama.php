ef5ee8bd357b73a0870d66b85640f58f61d10b70
FauzanAzmi12181 <83965576+FauzanAzmi12181@users.noreply.github.com>
2021-06-11 19:49:30 +0700

    Update hal_utama.php
    Pembaruan hal_utama V2

--- hal_utama.php
+++ hal_utama.php
@@ -3,79 +3,62 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class hal_utama extends CI_Controller {
 
-	public function index()
-	{
+    public function __construct(){
+        parent::__construct();
         $this->load->model('mymodel');
-        $dataProject = $this->mymodel->getData("project");
-        $data = array(
-            "dataMu" => $dataProject
-        );
+    }
 
-        $this->load->view("Home", $data);
+	public function index()
+    {
+        $this->load->view("form_login");
 	}
 
-	public function baca_form(){
-        $this->load->view('form_tambah');
-    }  
+    public function aksi_login()
+    {
+        $Username = $this->input->post('Username');
+        $Password = md5($this->input->post('Password'));
 
-    public function tambah_data(){
-        $this->load->model('mymodel');
-        $dataInputan = array(
-            'Alat' => $this->input->post('Alat'),
-            'Fungsi' => $this->input->post('Fungsi'),
-        );
-
-        $this->mymodel->masukkan('project', $dataInputan);
-        redirect(base_url(), 'refresh');
-    }
-    
-    public function hapus_data($penunjuk){
         $dataPenunjuk = array(
-            'Nomor' => $penunjuk
-        );
-
-        $this->load->model('mymodel');
-        $this->mymodel->hapus('project', $dataPenunjuk);
-        redirect(base_url(), 'refresh');
-    }
-
-    public function ambil_dataWhere($penunjuk){
-        $dataPenunjuk = array(
-            'Nomor' => $penunjuk
+            'Username' => $Username,
+            'Password' => $Password,
         );
         
-        $this->load->model('mymodel');
-        $dataProject = $this->mymodel->getData_khusus("project", $dataPenunjuk);
-        $data = array(
-            "dataMu" => $dataProject
-        );
+        // $dataProject = $this->mymodel->getData_khusus("dUser", $dataPenunjuk);
+        // var_dump($dataProject);
 
-        $this->load->view("form_edit", $data);
+        $cek = count($this->mymodel->getData_khusus("dUser", $dataPenunjuk));
+        // echo $cek;
+
+        if ($cek > 0){
+            $data_session = array(
+                'nama' => $Username,
+                'status' => "login"
+            );
+            $this->session->set_userdata($data_session);
+
+            redirect(base_url()."index.php/hal_admin");
+        }
+        else{
+            redirect(base_url());
+        }
+	}
+
+    public function daftar(){
+        $this->load->view("form_daftar");
     }
-    
-    public function update_data(){
+
+    public function aksi_daftar(){
         $dataInputan = array(
-            'Alat' => $this->input->post('Alat'),
-            'Fungsi' => $this->input->post('Fungsi'),
+            'Username' => $this->input->post('Username'),
+            'password' => md5($this->input->post('Password')),
         );
 
-        $dataPenunjuk = array(
-            'Nomor' => $this->input->post('Nomor')
-        );
-        $this->load->model('mymodel');
-        $dataProject = $this->mymodel->perbarui("project", $dataInputan, $dataPenunjuk);
+        $this->mymodel->masukkan('dUser', $dataInputan);
         redirect(base_url(), 'refresh');
     }
-    // public function fungsi($param){
-	// 	// echo "Hello World".$param;
-	// 	$this->load->view('Home');
-	// }
 
-	// public function param($nama){
-	// // 	echo "Selamat Datang kepada ".$nama;
-	// $data2 = array(
-	// 	'nama' => $nama
-	// );
-	// $this->load->view('Home', $data2);
-	// }
+    public function logout(){
+       $this->session->sess_destroy();
+       redirect(base_url());
+    }
 }
