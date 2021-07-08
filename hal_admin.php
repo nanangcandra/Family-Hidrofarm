e305f2ad414cae2d570ffffb43b48a0269929e9b
FauzanAzmi12181 <83965576+FauzanAzmi12181@users.noreply.github.com>
2021-07-08 19:19:12 +0700

    Update hal_admin.php

--- hal_admin.php
+++ hal_admin.php
@@ -1,15 +1,13 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class hal_admin extends CI_Controller
-{
+class hal_admin extends CI_Controller {
 
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
         $this->load->model('mymodel');
 
-        if ($this->session->userdata('status') != "login") {
+        if($this->session->userdata('status') != "login"){
             redirect(base_url());
         }
     }
@@ -17,43 +15,40 @@ class hal_admin extends CI_Controller
     public function index()
     {
         $dataProject = $this->mymodel->getData("project");
-
+            
         $data = array(
             "dataMu" => $dataProject
         );
-
-        $this->load->view("Home", $data);
-    }
-
-    public function baca_form()
-    {
-        $this->load->view('form_tambah');
-    }
     
-    public function tambah_data()
-    {
+        $this->load->view("Home", $data);
+	}
+
+	public function baca_form(){
+        $this->load->view('form_tambah');
+    }  
+
+    public function tambah_data(){
         $dataInputan = array(
             'NamaBarang' => $this->input->post('NamaBarang'),
             'JenisBarang' => $this->input->post('JenisBarang'),
-            'Stok' => $this->input->post('Stok'),
+            'Volume' => $this->input->post('Volume'),
             'Harga' => $this->input->post('Harga'),
+            'Stok' => $this->input->post('Stok'),
         );
 
         $this->mymodel->masukkan('project', $dataInputan);
-        redirect(base_url() . "index.php/hal_admin/");
+        redirect(base_url()."index.php/hal_admin/");
     }
-
-    public function hapus_data($penunjuk)
-    {
+    
+    public function hapus_data($penunjuk){
         $dataPenunjuk = array(
             'Nomor' => $penunjuk
         );
         $this->mymodel->hapus('project', $dataPenunjuk);
-        redirect(base_url() . "index.php/hal_admin/");
+        redirect(base_url()."index.php/hal_admin/");
     }
 
-    public function ambil_dataWhere($penunjuk)
-    {
+    public function ambil_dataWhere($penunjuk){
         $dataPenunjuk = array(
             'Nomor' => $penunjuk
         );
@@ -64,20 +59,34 @@ class hal_admin extends CI_Controller
 
         $this->load->view("form_edit", $data);
     }
-
-    public function update_data()
-    {
+    
+    public function update_data(){
         $dataInputan = array(
             'NamaBarang' => $this->input->post('NamaBarang'),
             'JenisBarang' => $this->input->post('JenisBarang'),
-            'Stok' => $this->input->post('Stok'),
+            'Volume' => $this->input->post('Volume'),
             'Harga' => $this->input->post('Harga'),
+            'Stok' => $this->input->post('Stok'),
         );
 
         $dataPenunjuk = array(
             'Nomor' => $this->input->post('Nomor')
         );
         $dataProject = $this->mymodel->perbarui("project", $dataInputan, $dataPenunjuk);
-        redirect(base_url() . "index.php/hal_admin/");
+        redirect(base_url()."index.php/hal_admin/");
+    }
+
+    public function form_daftarAPI(){
+        $this->load->view('form_daftarAPI');
+    }  
+
+    public function aksi_daftarAPI(){
+        $dataInputan = array(
+            'Nama' => $this->input->post('Nama'),
+            'Auth' => $this->input->post('kode'),
+        );
+
+        $this->mymodel->masukkan('user_api', $dataInputan);
+        redirect(base_url()."index.php/hal_admin/");
     }
 }
