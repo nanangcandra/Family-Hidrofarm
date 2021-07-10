8f417c3d8208ba235abe725da6aaaba875e97520
nanangcandra <83944937+nanangcandra@users.noreply.github.com>
2021-07-10 22:52:27 +0700

    Home.php

--- Home.php
+++ Home.php
@@ -1,137 +1,140 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Family Hidrofarm</title>
-        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-        <link href="<?php echo base_url()."assets/css/styles.css" ?>" rel="stylesheet" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">Family Hidrofarm</a>
-            <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-            <!-- Navbar Search-->
-            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                <div class="input-group">
-                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Family Hidrofarm</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+    <link href="<?php echo base_url() . "assets/css/styles.css" ?>" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <!-- Navbar Brand-->
+        <a class="navbar-brand ps-3" href="index.html">Family Hidrofarm</a>
+        <!-- Sidebar Toggle-->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+        <!-- Navbar Search-->
+        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+            <div class="input-group">
+                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+            </div>
+        </form>
+        <!-- Navbar-->
+        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="<?php echo base_url() . "index.php/hal_utama/logout/"; ?>">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Main Dashboard</div>
+                        <a class="nav-link" href="<?php echo base_url() . "index.php/hal_admin"; ?>">
+                            <div class="sb-nav-link-icon"></div>
+                            Daftar Barang Penjualan
+                        </a>
+                        <a class="nav-link" href="<?php echo base_url() . "index.php/hal_admin/form_daftarAPI"; ?>">
+                            <div class="sb-nav-link-icon"></div>
+                            Autentifikasi API
+                        </a>
+                    </div>
                 </div>
-            </form>
-            <!-- Navbar-->
-            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="<?php echo base_url()."index.php/hal_utama/logout/"; ?>">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Main Dashboard</div>
-                            <a class="nav-link" href="<?php echo base_url()."index.php/hal_admin"; ?>">
-                                <div class="sb-nav-link-icon"></div>
-                                Daftar Barang Penjualan
-                            </a>
-                            <a class="nav-link" href="<?php echo base_url()."index.php/hal_admin/form_daftarAPI"; ?>">
-                                <div class="sb-nav-link-icon"></div>
-                                Autentifikasi API
-                            </a>
-                        </div>
-                    </div>
-                    <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Family Hidrofarm
-                    </div>
-                </nav>
-            </div>
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid px-4">
-                        <h1 class="mt-4">FAMILY HIDROFARM</h1>
-                        <div class="card mb-4">
-                            <div class="card-body">
-                            Family Hidrofarm adalah toko online hidroponik yang menjual berbagai macam tanaman hidroponik, mulai dari menjual bibit atau benih dan perlengkapan-perlengkapan lain seperti alat atau media hidroponik.
-                            </div>
-                        </div>
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fas fa-table me-1"></i>
-                                <b>Daftar Barang Penjualan</b><br><br>
-                                <a class="btn btn-primary" href="<?php echo base_url()."index.php/hal_admin/baca_form"; ?>">Tambah Barang</a>
-                            </div>
-                            <div class="card-body">
-                                <table id="datatablesSimple">
-                                    <thead>
-                                        <tr>
-                                            <th>Nama Barang</th>
-                                            <th>Kategori</th>
-                                            <th>Volume</th>
-                                            <th>Harga</th>
-                                            <th>Stok</th>
-                                            <th>Aksi</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>Nama Barang</th>
-                                            <th>Kategori</th>
-                                            <th>Volume</th>
-                                            <th>Harga</th>
-                                            <th>Stok</th>
-                                            <th>Aksi</th>
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
-                                        <?php
-                                            foreach($dataMu as $dat){
-                                        ?>
-                                            <tr>
-                                                <td><?php echo $dat['NamaBarang']; ?></td>
-                                                <td><?php echo $dat['JenisBarang']; ?></td>
-                                                <td><?php echo $dat['Volume']; ?></td>
-                                                <td><?php echo $dat['Harga']; ?></td>
-                                                <td><?php echo $dat['Stok']; ?></td>
-                                                <td>
-                                                    <a class="btn btn-primary" href="<?php echo base_url()."index.php/hal_admin/hapus_data/".$dat['Nomor']; ?>">Delete</a>
-                                                    <a class="btn btn-primary" href="<?php echo base_url()."index.php/hal_admin/ambil_dataWhere/".$dat['Nomor']; ?>">Edit</a>
-                                                </td>
-                                            </tr>
-                                        <?php } ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2021</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    Family Hidrofarm
+                </div>
+            </nav>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="<?php echo base_url()."assets/js/scripts.js" ?>"></script>
-        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-        <script src="<?php echo base_url()."assets/js/datatables-simple-demo.js" ?>"></script>
-    </body>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">FAMILY HIDROFARM</h1>
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            Family Hidrofarm adalah toko online hidroponik yang menjual berbagai macam tanaman hidroponik, mulai dari menjual bibit atau benih dan perlengkapan-perlengkapan lain seperti alat atau media hidroponik.
+                        </div>
+                    </div>
+                    <div>
+                        <div class="card-header">
+                            <i class="fas fa-table me-1"></i>
+                            <b>Daftar Barang Penjualan</b><br><br>
+                            <a class="btn btn-primary" href="<?php echo base_url() . "index.php/hal_admin/baca_form"; ?>">Tambah Barang</a>
+                        </div>
+                        <div class="card-body">
+                            <table id="datatablesSimple">
+                                <thead>
+                                    <tr>
+                                        <th>Nama Barang</th>
+                                        <th>Kategori</th>
+                                        <th>Volume</th>
+                                        <th>Harga</th>
+                                        <th>Stok</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tfoot>
+                                    <tr>
+                                        <th>Nama Barang</th>
+                                        <th>Kategori</th>
+                                        <th>Volume</th>
+                                        <th>Harga</th>
+                                        <th>Stok</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </tfoot>
+                                <tbody>
+                                    <?php
+                                    foreach ($dataMu as $dat) {
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $dat['NamaBarang']; ?></td>
+                                            <td><?php echo $dat['JenisBarang']; ?></td>
+                                            <td><?php echo $dat['Volume']; ?></td>
+                                            <td><?php echo $dat['Harga']; ?></td>
+                                            <td><?php echo $dat['Stok']; ?></td>
+                                            <td>
+                                                <a class="btn btn-primary" href="<?php echo base_url() . "index.php/hal_admin/hapus_data/" . $dat['Nomor']; ?>"> <i class="fas fa-trash-alt"></i></a>
+                                                <a class="btn btn-primary" href="<?php echo base_url() . "index.php/hal_admin/ambil_dataWhere/" . $dat['Nomor']; ?>"><i class="fas fa-edit"></i></a>
+                                            </td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2021</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url() . "assets/js/scripts.js" ?>"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url() . "assets/js/datatables-simple-demo.js" ?>"></script>
+</body>
+
 </html>
